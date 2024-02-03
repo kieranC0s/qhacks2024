57d4a4d0f3866ee2d6b92bf38c774a52d839ddbc
Mile Stosic <100725670+miodrag4@users.noreply.github.com>
2024-02-03 13:40:50 -0500

    Add files via upload

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,58 @@
-import React, { useState } from 'react';
-import './navbar.css'; // Make sure to create a corresponding CSS file
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { HashLink } from 'react-router-hash-link';
+import './NavBar.css'; // Make sure to create a corresponding CSS file
 
-function NavBar() {
-  const [isMenuOpen, setMenuOpen] = useState(false);
+// Place your asset imports here
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
+export const NavBar = () => {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
 
   return (
-    <nav className="navbar">
-      <div className="menu-button" onClick={toggleMenu}>
-        ☰
-      </div>
-      <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
-        <ul>
-          <li onClick={toggleMenu}>Summarize Notes</li>
-          <li onClick={toggleMenu}>Flash Cards</li>
-          <li onClick={toggleMenu}>Mock Tests</li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={HashLink} to="#home" onClick={() => onUpdateActiveLink('home')} className={activeLink === 'home' ? 'active' : ''}>Home</Nav.Link>
+            {/* Add more Nav.Link items here */}
+          </Nav>
+          <div className="social-icon">
+            {/* Add your social media links here */}
+            <a href={navIcon1} target="_blank" rel="noopener noreferrer">
+              <img src={navIcon1} alt="LinkedIn" />
+            </a>
+            {/* ... other social icons */}
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
-
-export default NavBar;
